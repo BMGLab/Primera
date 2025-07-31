f0a03f6bacc1f6e2e24f038b214f69fcb4a1c5a3
musakrgzn <musakirgezen@hotmail.com>
2025-07-31 14:59:52 +0300

    Update filter.py

--- primera/filter.py
+++ primera/filter.py
@@ -1,26 +1,31 @@
 import pandas as pd
 
-def parse_csv_file(csv_path, allowed_chr_list):
-    df = pd.read_csv(csv_path, comment='#')  
+def parser(psl_path, chr_list):
+    cols = [
+        "match", "mis-match", "rep-match", "N's", "Q gap count", "Q gap bases",
+        "T gap count", "T gap bases", "strand",
+        "Q name", "Q size", "Q start", "Q end",
+        "T name", "T size", "T start", "T end",
+        "block count", "blockSizes", "qStarts", "tStarts"
+    ]
 
-    if "Q name" not in df.columns or "T name" not in df.columns:
-        print("error!")
-        return
+    df = pd.read_csv(psl_path, sep="\t", header = None, names = cols, skiprows=4)
 
     grouped = df.groupby("Q name")
 
-    print("\n RESULTs(only this chr's:", allowed_chr_list, ")\n")
+    print("\nRESULTS :", chr_list), "\n"
 
     for group_name, group_df in grouped:
         t_names = set(group_df["T name"].unique())
-        if t_names.issubset(allowed_chr_list):
-            print(f"# Group: {group_name}")
-
+        if t_names.issubset(chr_list):
+            print(f"#Group: {group_name}")
+            print(group_df.to_string(index=False))
+            print()
 
 if __name__ == "__main__":
-    csv_file = input(" enter csv filepath: ").strip()
-    chr_input = input(" want chrs(ex: chr3,chr7,chr8): ").strip()
+    psl_file = input("Enter PSL file path: ").strip()
+    chr_input = input("Enter chrs: ").strip()
 
-    allowed_chr = set([c.strip() for c in chr_input.split(",") if c.strip()])
+    chrs = set(c.strip() for c in chr_input.split(",") if c.strip())
 
-    parse_csv_file(csv_file, allowed_chr)
+    parser(psl_file, chrs)
