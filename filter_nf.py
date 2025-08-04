b4e3bcad16508d25122fdd573714a382c4671b6a
musakrgzn <musakirgezen@hotmail.com>
2025-08-04 17:50:47 +0300

    Update filter_nf.py

--- filter_nf.py
+++ filter_nf.py
@@ -15,7 +15,6 @@ def parser(psl_path, chr_list, output_txt):
     with open(output_txt, "w") as out:
         for group_name, group_df in grouped:
             t_names = set(group_df["T name"].unique())
-
             if t_names == (chr_list):
                 out.write(f"{group_name}\n")
 
