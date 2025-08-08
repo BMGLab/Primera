530f5aa445b290c432dbfbf57f79d6b5f6c42c58
Musa KIRGEZEN <musakirgezen@hotmail.com>
2025-08-08 16:47:21 +0300

    Update url.py

--- primera/url.py
+++ primera/url.py
@@ -1,6 +1,6 @@
 template_url = "https://genome.ucsc.edu/cgi-bin/hgPcr?hgsid=2900325362_8e48BzUFKDYcxxsPnAlLNmzHKyGA&org=Human&db=hg38&wp_target=genome&wp_f={f}&wp_r={r}&Submit=Submit&wp_size=300&wp_perfect=15&wp_good=15&boolshad.wp_flipReverse=0&wp_append=on&boolshad.wp_append=0"
 
-input_file = "/home/musa/Desktop/Primera_mk/work/9b/3992f3e1375fef6fdc143c01275de2/primer_result_filtered.txt"
+input_file = "primer_result_filtered.txt"
 
 output_file = "primers_with_urls.txt"
 
