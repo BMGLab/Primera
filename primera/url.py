42d95a8def959288bcfbfd0ffa2862c6a15ddd37
Musa KIRGEZEN <musakirgezen@hotmail.com>
2025-08-11 13:21:02 +0300

    Update url.py

--- primera/url.py
+++ primera/url.py
@@ -1,22 +1,21 @@
+import sys
+import pandas as pd
+
+input_file = sys.argv[1]
+output_file = sys.argv[2]
+
+rows = []
 template_url = "https://genome.ucsc.edu/cgi-bin/hgPcr?hgsid=2900325362_8e48BzUFKDYcxxsPnAlLNmzHKyGA&org=Human&db=hg38&wp_target=genome&wp_f={f}&wp_r={r}&Submit=Submit&wp_size=300&wp_perfect=15&wp_good=15&boolshad.wp_flipReverse=0&wp_append=on&boolshad.wp_append=0"
 
-input_file = "primer_result_filtered.txt"
-
-output_file = "primers_with_urls.txt"
-
-with open(input_file, "r") as infile, open(output_file, "w") as outfile:
-    header = next(infile)  
-    outfile.write(header.strip() + "\t")  
-    
+with open(input_file, "r") as infile:
     for line in infile:
         parts = line.strip().split("\t")
         if len(parts) < 4:
-            outfile.write(line)  
             continue
-        
         forward = parts[2]
         reverse = parts[3]
         url = template_url.format(f=forward, r=reverse)
-        
-        
-        outfile.write(line.strip() + "\t" + url + "\n")
+        rows.append(parts + [url])
+
+
+pd.DataFrame(rows).to_excel(output_file, index=False, header=False)
